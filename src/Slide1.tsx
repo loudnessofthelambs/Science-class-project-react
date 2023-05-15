659df1f789402a9bc5b62f299cb5455301c1af28
Hirad <hirad.dehghani@gmail.com>
2023-05-15 18:03:30 -0400

    Added some css

--- src/Slide1.tsx
+++ src/Slide1.tsx
@@ -6,6 +6,73 @@ const Ecosystem = () => {
 		<div className="ecosystem">
 			<h1>Ecosystem</h1>
 			<img src="image.jpg" alt="Pacific ocean" />
+			<table className="styled-table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Picture</th>
+						<th>Type</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Fishy</td>
+						<td>N/A</td>
+						<td>Biotic</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+					<tr>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+						<td>Lorem.</td>
+					</tr>
+				</tbody>
+			</table>
+
 		</div>
 	);
 }
