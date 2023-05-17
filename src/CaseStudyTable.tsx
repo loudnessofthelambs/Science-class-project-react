4d649a62e43e59a89a8285a5a32cdb651f93380b
loudnessofthelambs <109435021+loudnessofthelambs@users.noreply.github.com>
2023-05-17 13:00:30 -0400

    Finished slide 2

--- src/CaseStudyTable.tsx
+++ src/CaseStudyTable.tsx
@@ -7,7 +7,7 @@ const CaseStudyTable = () => {
                     <th>Air</th>
                     <th>Food</th>
                     <th>Energy</th>
-                    <th>habitat</th>
+                    <th>Habitat</th>
                 </tr>
             </thead>
             <tbody>
@@ -15,8 +15,9 @@ const CaseStudyTable = () => {
                     <div className="td-container">
                         <td>They mainly get their water from their food/pray, but they can occasionally drink seawater while feeding. They also live in the water, and hunt in it too.</td>
                         <td>To get their air, they breathe through nostrils on the top of their head called "blowholes", and go to the top of the surface to use their nostrils. Their oxygen is produced by plants such as phytoplankton.</td>
-                        <td>Food </td>
-
+                        <td>Whales normally are carnivores, eating smaller fish. They eat their prey in the water, which is also where they live.</td>
+                        <td>Whales usually get their energy, like most other animals, by eating their food.</td>
+                        <td>Whales live in the water, in small groups of around 30 whales called a pod.</td>
                     </div>
                 </tr>
             </tbody>
